a21c8f46481a5c0be88f60fca9c39eba1c40921f
shakadak <nathanael.pineau@gmail.com>
2015-03-23 17:04:54 +0100

    correcting color

--- src/ini/new_color.c
+++ src/ini/new_color.c
@@ -6,19 +6,19 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/22 18:40:32 by npineau           #+#    #+#             */
-/*   Updated: 2015/03/22 16:02:34 by npineau          ###   ########.fr       */
+/*   Updated: 2015/03/23 17:04:26 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libumlx.h"
 
-t_color	new_color(char const r, char const g, char const b, char const a)
+t_color	new_color(char const b, char const g, char const r, char const a)
 {
 	t_color	new;
 
-	new.rgb[0] = r;
+	new.rgb[0] = b;
 	new.rgb[1] = g;
-	new.rgb[2] = b;
+	new.rgb[2] = r;
 	new.rgb[3] = a;
 	return (new);
 }
