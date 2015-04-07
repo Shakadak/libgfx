d8ff5a858e8a952c9673d9cfb01e99ab79bd7593
shakadak <nathanael.pineau@gmail.com>
2015-04-07 18:04:29 +0200

    80 column

--- src/pix/put_pixel_to_image.c
+++ src/pix/put_pixel_to_image.c
@@ -6,11 +6,10 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/22 13:58:43 by npineau           #+#    #+#             */
-/*   Updated: 2015/03/22 16:05:44 by npineau          ###   ########.fr       */
+/*   Updated: 2015/04/07 18:04:12 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>////////////
 #include "libft.h"
 #include "libumlx.h"
 
@@ -19,10 +18,8 @@ void	put_pixel_to_image(t_img const img,
 {
 	int	pos;
 
-/*	if (pixel.pos.x < 0 || pixel.pos.x >= img.dim.x
-			|| pixel.pos.y < 0 || pixel.pos.y >= img.dim.x)
-		return ;*/
 	pos = pixel.pos.y * img.lsize
 	+ pixel.pos.x * (img.bits / 8);
-	ft_memcpy(img.data + pixel.pos.y * img.lsize + pixel.pos.x * img.bits / 8, &pixel.color, img.bits / 8);
+	ft_memcpy(img.data + pixel.pos.y * img.lsize + pixel.pos.x * img.bits / 8,
+			&pixel.color, img.bits / 8);
 }
