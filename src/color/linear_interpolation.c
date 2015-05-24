32f43ddfe44879f3a62747231c3983d62c92d064
shakadak <nathanael.pineau@gmail.com>
2015-05-24 15:39:06 +0200

    some simplification

--- src/color/linear_interpolation.c
+++ src/color/linear_interpolation.c
@@ -6,20 +6,25 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/22 14:11:09 by npineau           #+#    #+#             */
-/*   Updated: 2015/05/23 18:08:46 by npineau          ###   ########.fr       */
+/*   Updated: 2015/05/24 15:38:41 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libumlx.h"
 
-t_color	linear_interpolation(
+static double	interpolate(double const a, double const b, double const f)
+{
+	return ((b - a) * f + a);
+}
+
+t_color			linear_interpolation(
 		t_color const src,
 		t_color const end,
 		double const factor)
 {
 	return (new_color(
-	(end.rgb[0] - src.rgb[0]) * factor + src.rgb[0],
-	(end.rgb[1] - src.rgb[1]) * factor + src.rgb[1],
-	(end.rgb[2] - src.rgb[2]) * factor + src.rgb[2],
-	(end.rgb[3] - src.rgb[3]) * factor + src.rgb[3]));
+	interpolate(src.rgb[0], end.rgb[0], factor),
+	interpolate(src.rgb[1], end.rgb[1], factor),
+	interpolate(src.rgb[2], end.rgb[2], factor),
+	interpolate(src.rgb[3], end.rgb[3], factor)));
 }
